16f04cf7164a407e09c92b4efacdfd94814638f9
Heero.Luo <heeroluo@gmail.com>
2015-03-08 11:36:38 +0800

    在微信中点击微信分享时显示文字提示而不是二维码

--- public/script/app/common/share/1.0.x/index-debug.js
+++ public/script/app/common/share/1.0.x/index-debug.js
@@ -1,6 +1,6 @@
 /*!
  * LetsBlog
- * SNS share - v1.0.0 (2015-02-22T15:56:43+0800)
+ * SNS share - v1.0.0 (2015-03-08T11:14:47+0800)
  * Released under MIT license
  */
 define(function(require, exports, module) { 'use strict';
@@ -32,22 +32,36 @@ var shareTypes = {
 			}
 		});
 
-		var layer = $(
-			'<div class="share-wechat">' +
-				'<p>扫一扫，分享到微信</p>' +
-				'<p class="share-wechat__qrcode"></p>' +
-				'<p class="share-wechat__close">点击任意位置关闭</p>' +
-			'</div>'
-		);
-		$('body').append(layer);
+		var layer;
+		if (/MicroMessenger/.test(window.navigator.userAgent)) {
+			layer = $(
+				'<div class="share-wechat--inwechat clearfix">' +
+					'<div class="share-wechat--inwechat__text">点击右上角按钮进行分享<br />(点击任意位置关闭提示)</div>' +
+					'<div class="share-wechat--inwechat__arrow">' +
+						'<span class="share-wechat--inwechat__arrow__triangle"></span>' +
+						'<span class="share-wechat--inwechat__arrow__rectangle"></span>' +
+					'</div>' +
+				'</div>'
+			);
+			$('body').append(layer);
+		} else {
+			layer = $(
+				'<div class="share-wechat">' +
+					'<p>扫一扫，分享到微信</p>' +
+					'<p class="share-wechat__qrcode"></p>' +
+					'<p class="share-wechat__close">点击任意位置关闭</p>' +
+				'</div>'
+			);
+			$('body').append(layer);
 
-		var qrcode = new QRCode(layer.find('.share-wechat__qrcode').get(0), {
-		    text: params.url,
-		    width: 128,
-		    height: 128,
-		    colorDark : '#000000',
-		    colorLight : '#ffffff'
-		});
+			var qrcode = new QRCode(layer.find('.share-wechat__qrcode').get(0), {
+				text: params.url,
+				width: 128,
+				height: 128,
+				colorDark : '#000000',
+				colorLight : '#ffffff'
+			});
+		}
 
 		function hide() {
 			layer.remove();
