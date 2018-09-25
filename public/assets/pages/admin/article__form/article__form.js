45859d793643083ffc8e4530ec457697021386c1
Heero.Law <heeroluo@gmail.com>
2018-09-25 17:14:36 +0800

    修改为新的模板引用方式

--- public/assets/pages/admin/article__form/article__form.js
+++ public/assets/pages/admin/article__form/article__form.js
@@ -1,30 +1,36 @@
-var base = require('lib/base@1.1'),
-	$ = require('lib/dom@1.1'),
-	ajax = require('lib/ajax@1.3'),
-	Validator = require('lib/validator@1.1'),
-	xTpl = require('common/xtpl/xtpl'),
-	CKEDITOR = window.CKEDITOR;
+const base = require('lib/base@1.1');
+const $ = require('lib/dom@1.1');
+const ajax = require('lib/ajax@1.3');
+const Validator = require('lib/validator@1.1');
+const xTpl = require('xtpl/xtpl');
+const CKEDITOR = window.CKEDITOR;
 
 
 // 防止误刷新页面
-window.onbeforeunload = function() { return '确定要离开此页面？'; };
+window.onbeforeunload = () => { return '确定要离开此页面？'; };
 
 
 // 页面类型: true表示编辑文章页面, false表示创建文章页面
-var isUpdatePage = Number($('input[name=articleid]').val()) > 0;
+let isUpdatePage = Number($('input[name=articleid]').val()) > 0;
 
 
 new Validator({
 	form: $('#article-form'),
 	steps: [
-		{ fields: 'title', message: '请填写标题' },
+		{
+			fields: 'title',
+			message: '请填写标题'
+		},
 		{
 			fields: 'title_en',
 			message: '英文标题只能包含英文、数字和中划线',
 			rule: function(val) { return /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(val); },
 			required: false
 		},
-		{ fields: 'categoryid', message: '请选择分类' }
+		{
+			fields: 'categoryid',
+			message: '请选择分类'
+		}
 	],
 	events: base.extend({
 		beforesubmit: function() {
@@ -34,7 +40,7 @@ new Validator({
 	}, Validator.defaultOptions.events),
 	// AJAX提交
 	submitProxy: function(data, form) {
-		var submitBtn = $('#submit-btn'), btnText = submitBtn.val();
+		const submitBtn = $('#submit-btn'), btnText = submitBtn.val();
 		submitBtn.prop('disabled', true).val('提交中...');
 		ajax.send({
 			url: form.attr('action'),
@@ -68,14 +74,14 @@ new Validator({
 });
 
 
-var uploadPanel = $('#upload-panel'),
-	uploadControl = uploadPanel.find('input[type=file]'),
-	uploadResult = uploadPanel.find('.upload-result'),
-	uploadTasks = [ ];
+const uploadPanel = $('#upload-panel');
+const uploadControl = uploadPanel.find('input[type=file]');
+const uploadResult = uploadPanel.find('.upload-result');
+const uploadTasks = [];
 
 // 中断上传
 function cancelUpload(taskId) {
-	var task = uploadTasks[taskId];
+	const task = uploadTasks[taskId];
 	if (task) {
 		task.abort();
 		uploadTasks[taskId] = null;
@@ -86,19 +92,19 @@ function cancelUpload(taskId) {
 uploadResult.on('click', function(e) {
 	e.preventDefault();
 
-	var self = $(this), confirmTimer = self.data('confirmTimer');
+	const self = $(this), confirmTimer = self.data('confirmTimer');
 	// 点击两次以确认取消
 	if (confirmTimer) {
 		clearTimeout(confirmTimer);
-		var parent = self.parent();
+		const parent = self.parent();
 		cancelUpload(parent.attr('data-taskid'));
 		parent.remove();
 	} else {
 		self.text('确定？');
-		self.data( 'confirmTimer', setTimeout(function() {
+		self.data('confirmTimer', setTimeout(function() {
 			// 如果没有进一步操作，则恢复原来的文字
 			self.text('取消').removeData('confirmTimer');
-		}, 3000) );
+		}, 3000));
 	}
 }, {
 	delegator: '.upload-cancel'
@@ -107,9 +113,9 @@ uploadResult.on('click', function(e) {
 // 插入链接
 uploadResult.on('click', function(e) {
 	e.preventDefault();
-	var text = window.prompt('请输入链接文字');
+	const text = window.prompt('请输入链接文字');
 	if (text) {
-		xTpl.render(require.resolve('./insert-link'), {
+		xTpl.render(_tpl('./insert-link.xtpl'), {
 			text: text,
 			path: $(this).siblings().filter('.upload-result-path').text()
 		}).then(function(html) {
@@ -123,12 +129,12 @@ uploadResult.on('click', function(e) {
 // 插入图片
 uploadResult.on('click', function(e) {
 	e.preventDefault();
-	xTpl.render(require.resolve('./insert-img'), {
+	xTpl.render(_tpl('./insert-img.xtpl'), {
 		alt: window.prompt('请输入图片说明') || '',
 		path: $(this).siblings().filter('.upload-result-path').text()
 	}).then(function(html) {
 		CKEDITOR.instances.content.insertHtml(html);
-	})
+	});
 }, {
 	delegator: '.upload-result-insertimg'
 });
@@ -137,7 +143,7 @@ uploadResult.on('click', function(e) {
 uploadControl.change(function(e) {
 	if (!this.files.length) { return; }
 
-	var file = this.files[0];
+	const file = this.files[0];
 	if (!file.size) {
 		alert('不能上传空文件');
 		return;
@@ -149,38 +155,38 @@ uploadControl.change(function(e) {
 	}
 
 	// 获取文件全名和扩展名
-	var fileName = file.name,
-		fileNameSegments = fileName.split('.'),
-		extName = fileNameSegments.length > 1 ?
-			fileNameSegments[fileNameSegments.length - 1].toLowerCase() : null;
+	const fileName = file.name;
+	const fileNameSegments = fileName.split('.');
+	const extName = fileNameSegments.length > 1 ?
+		fileNameSegments[fileNameSegments.length - 1].toLowerCase() : null;
 
-	var formData = new FormData();
+	const formData = new FormData();
 	formData.append('file', file);
 
-	var xhr = new XMLHttpRequest(), taskId = uploadTasks.push(xhr) - 1;
+	const xhr = new XMLHttpRequest(), taskId = uploadTasks.push(xhr) - 1;
 	xhr.open('POST', '/admin/article/attachment/upload');
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4) {
 			uploadTasks[taskId] = null;
 
-			var errMsg;
+			let errMsg, res;
 			if (this.status == 200) {
-				var res = JSON.parse(this.responseText);
+				res = JSON.parse(this.responseText);
 				if (res.status !== 1) { errMsg = res.message; }
 			} else {
 				errMsg = '状态码' + this.status;
 			}
 
-			var taskElement = uploadResult.find('p[data-taskid=' + taskId + ']').empty();
-			
-			var render;
+			const taskElement = uploadResult.find('p[data-taskid=' + taskId + ']').empty();
+
+			let render;
 			if (errMsg) {
-				render = xTpl.render(require.resolve('./upload-fail'), {
+				render = xTpl.render(_tpl('./upload-fail.xtpl'), {
 					fileName: fileName,
 					message: errMsg
 				});
 			} else {
-				render = xTpl.render(require.resolve('./upload-success'), {
+				render = xTpl.render(_tpl('./upload-success.xtpl'), {
 					fileName: fileName,
 					path: res.data.path,
 					isImage: ['jpg', 'jpeg', 'png', 'gif', 'bmp'].indexOf(extName) !== -1
@@ -194,7 +200,7 @@ uploadControl.change(function(e) {
 	};
 	xhr.send(formData);
 
-	xTpl.render(require.resolve('./uploading'), {
+	xTpl.render(_tpl('./uploading.xtpl'), {
 		fileName: fileName,
 		taskId: taskId
 	}).then(function(html) {
