889625a80c24ffc47ef327ee48409362a6f3ff8f
Heero.Law <heeroluo@gmail.com>
2018-09-25 18:34:51 +0800

    ES6化

--- public/assets/pages/user/login/login.js
+++ public/assets/pages/user/login/login.js
@@ -1,13 +1,19 @@
-var $ = require('lib/dom@1.1'),
-	ajax = require('lib/ajax@1.3'),
-	Validator = require('lib/validator@1.1');
+const $ = require('lib/dom@1.1');
+const ajax = require('lib/ajax@1.3');
+const Validator = require('lib/validator@1.1');
 
 
 new Validator({
 	form: $('#login-form'),
 	steps: [
-		{ fields: 'username', message: '请填写用户名' },
-		{ fields: 'password', message: '请填写密码' },
+		{
+			fields: 'username',
+			message: '请填写用户名'
+		},
+		{
+			fields: 'password',
+			message: '请填写密码'
+		},
 		{
 			fields: 'username',
 			message: '用户名必须为2-20个字母、数字或者下划线',
@@ -22,7 +28,7 @@ new Validator({
 			method: 'POST'
 		}).spread(function(res) {
 			if (res.status === 1) {
-				var referrer = data.filter(function(d) {
+				const referrer = data.filter(function(d) {
 					return d.name == 'referrer';
 				})[0];
 				window.location.href = referrer.value;
